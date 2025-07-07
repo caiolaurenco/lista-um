216af5e170d9dac1344e7e595723f5a4060ba18c
caiolaurenco <caio_laurenco@estudante.sesisenai.org.br>
2025-07-07 11:13:52 -0300

    UPDATE - exercicio 3

---
+++ exercicio03.php
@@ -0,0 +1,31 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Informe se é par ou impar</title>
+</head>
+<body>
+
+<form method="POST">
+    <label>Digite um número:</label>
+    <input type="number" name="numero" required>
+    <button type="submit">Verificar</button>
+</form>
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $numero = $_POST["numero"];
+
+    if ($numero > 0) {
+  echo "O número é positivo.";
+} elseif ($numero < 0) {
+  echo "O número é negativo.";
+} else {
+  echo "O número é zero.";
+}
+};
+
+?>
+
+</body>
+</html>
